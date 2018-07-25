c42dfe7d71d1f670ab84500ef976720ceab60c0a
Vishmita <vjadeja@icreativetechnolabs.com>
2018-07-25 20:58:03 +0530

    [ADD]POC customization

---
+++ mclane_customization/models/res_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    license_number = fields.Char('License Number')
+    license_file = fields.Binary('License File')
+    expiration_date = fields.Date('Expriration Date')
