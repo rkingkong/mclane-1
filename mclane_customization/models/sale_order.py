bf260796a4b26441a30a376e9e5d090cdd31b068
Vishmita <vjadeja@icreativetechnolabs.com>
2018-08-03 19:23:43 +0530

    Change in msrp margin calculation

--- mclane_customization/models/sale_order.py
+++ mclane_customization/models/sale_order.py
@@ -8,10 +8,10 @@ class SaleOrderLine(models.Model):
 
     @api.depends('product_uom_qty')
     def _compute_margin_msrp(self):
+
         for order_line in self:
             amount_margin_msrp = 0.0  # type: float
-            amount_margin_msrp += (order_line.product_id.margin_msrp * order_line.product_uom_qty)
-
+            amount_margin_msrp += (order_line.product_id.product_tmpl_id.margin_msrp * order_line.product_uom_qty)
             order_line.update({
                 'margin_msrp': amount_margin_msrp
             })
@@ -33,3 +33,4 @@ class SaleOrder(models.Model):
             })
 
     margin_msrp_total = fields.Float(compute='_compute_margin_msrp_total', string='Expected Margin MSRP')
+
