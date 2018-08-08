a311484e8ab358655e90752a390cc76c67a9b2ea
Vishmita <vjadeja@icreativetechnolabs.com>
2018-08-08 16:54:03 +0530

    Hide Cigarettes and Tobacco Category

--- mclane_customization/controllers/portal.py
+++ mclane_customization/controllers/portal.py
@@ -3,16 +3,114 @@ from odoo.http import Controller, request, route
 from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.addons.website.controllers.main import Website
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.website_sale.controllers.main import TableCompute
 from odoo import http
+from werkzeug.exceptions import NotFound
+from odoo.addons.website.controllers.main import QueryURL
+from odoo.addons.http_routing.models.ir_http import slug
 import base64
+
+PPG = 20  # Products Per Page
+PPR = 4  # Products Per Row
 
 
 class WebsiteSaleCustom(WebsiteSale):
-    @http.route('/shop', type="http", auth="public", website=True)
+    @http.route([
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>'
+    ], type='http', auth="public", website=True)
     def shop(self, page=0, category=None, search='', ppg=False, **post):
         if request.session.uid is None:
             return request.redirect('/web/login')
-        return super(WebsiteSaleCustom, self).shop(page, category, search, ppg, **post)
+        if ppg:
+            try:
+                ppg = int(ppg)
+            except ValueError:
+                ppg = PPG
+            post["ppg"] = ppg
+        else:
+            ppg = PPG
+
+        if category:
+            category = request.env['product.public.category'].search([('id', '=', int(category))], limit=1)
+            if not category:
+                raise NotFound()
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [[int(x) for x in v.split("-")] for v in attrib_list if v]
+        attributes_ids = {v[0] for v in attrib_values}
+        attrib_set = {v[1] for v in attrib_values}
+
+        domain = self._get_search_domain(search, category, attrib_values)
+
+        keep = QueryURL('/shop', category=category and int(category), search=search, attrib=attrib_list,
+                        order=post.get('order'))
+
+        compute_currency, pricelist_context, pricelist = self._get_compute_currency_and_context()
+
+        request.context = dict(request.context, pricelist=pricelist.id, partner=request.env.user.partner_id)
+
+        url = "/shop"
+        if search:
+            post["search"] = search
+        if attrib_list:
+            post['attrib'] = attrib_list
+
+        partner = request.env.user.partner_id
+        res_cat_flex = request.env['res.partner.flexible.cat']
+        Display_Products = res_cat_flex.search([('csr_review', '=', False), ('partner_id', '=', partner.id)])
+        categ_ids = []
+        for categ_id in Display_Products:
+            categ_ids.append(categ_id.product_category.id)
+
+        categs = request.env['product.public.category'].search([('parent_id', '=', False), ('id', 'not in', categ_ids)])
+        Product = request.env['product.template']
+
+        parent_category_ids = []
+        if category and category.id not in categ_ids:
+            url = "/shop/category/%s" % slug(category)
+            parent_category_ids = [category.id]
+            current_category = category
+            while current_category.parent_id:
+                parent_category_ids.append(current_category.parent_id.id)
+                current_category = current_category.parent_id
+
+        domain += [('public_categ_ids.id', 'not in', categ_ids)]
+
+        product_count = Product.search_count(domain)
+        pager = request.website.pager(url=url, total=product_count, page=page, step=ppg, scope=7, url_args=post)
+        products = Product.search(domain, limit=ppg, offset=pager['offset'], order=self._get_search_order(post))
+
+        ProductAttribute = request.env['product.attribute']
+        if products:
+            # get all products without limit
+            selected_products = Product.search(domain, limit=False)
+            attributes = ProductAttribute.search([('attribute_line_ids.product_tmpl_id', 'in', selected_products.ids)])
+        else:
+            attributes = ProductAttribute.browse(attributes_ids)
+
+        values = {
+            'search': search,
+            'category': category,
+            'attrib_values': attrib_values,
+            'attrib_set': attrib_set,
+            'pager': pager,
+            'pricelist': pricelist,
+            'products': products,
+            'search_count': product_count,  # common for all searchbox
+            'bins': TableCompute().process(products, ppg),
+            'rows': PPR,
+            'categories': categs,
+            'attributes': attributes,
+            'compute_currency': compute_currency,
+            'keep': keep,
+            'parent_category_ids': parent_category_ids,
+        }
+        if category:
+            values['main_object'] = category
+        return request.render("website_sale.products", values)
 
 
 class WebsiteExtendCustom(Website):
